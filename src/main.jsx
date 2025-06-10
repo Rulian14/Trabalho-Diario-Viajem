587d55e84c95081c746fe137832ac86eb9c02ec3
Rulian14 <Rulian1403@gmail.com>
2025-06-10 18:12:54 -0300

    Tudo instalado e pronto!

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const root = document.getElementById("root");
+
+createRoot(document.getElementById("root")).render();
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
     <App />
-  </StrictMode>,
-)
+  </BrowserRouter>
+);
