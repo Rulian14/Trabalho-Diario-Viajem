6a295432d95bba2c55be03ad6b60bcbcb6dfc506
Rulian14 <Rulian1403@gmail.com>
2025-06-10 18:58:44 -0300

    Atualização dos Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,35 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/LoginUser",
+    element: <LoginUser />,
+  },
+  {
+    path: "/MenuUser",
+    element: <MenuUser />,
+  },
+  {
+    path: "UploadDiary",
+    element: <UploadDiary />,
+  },
+]);
 
 const root = document.getElementById("root");
 
-createRoot(document.getElementById("root")).render();
-ReactDOM.createRoot(root).render(
-  <BrowserRouter>
-    <App />
-    <LoginUser />
-    <MenuUser />
-    <UploadDiary />
-  </BrowserRouter>
+ReactDOM.createRoot(root).render(<RouterProvider router={router} />);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
 );
