bb74ee69bc45a1c9c62c334bd3b95f153512e500
Rulian14 <Rulian1403@gmail.com>
2025-06-10 18:44:47 -0300

    Criação de paginas

--- src/main.jsx
+++ src/main.jsx
@@ -10,5 +10,8 @@ createRoot(document.getElementById("root")).render();
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
     <App />
+    <LoginUser />
+    <MenuUser />
+    <UploadDiary />
   </BrowserRouter>
 );
---
+++ src/pages/LoginUser.jsx
@@ -0,0 +1,2 @@
+function LoginUser() {}
+export default LoginUser;
