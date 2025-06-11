6840bfb1dd90cee9146dffa645cc1d18380a0e9c
Rulian14 <Rulian1403@gmail.com>
2025-06-10 21:31:46 -0300

    estilização do header e correções

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,16 @@ function Header() {
   const navigate = useNavigate();
 
   return (
-    <div className="">
-      <button
-        className="border-2 p-2 rounded-md"
-        onClick={() => navigate("/LoginUser")}
-      >
-        Login
-      </button>
+    <div className="w-screen h-[50px] bg-amber-400 flex justify-end pr-5 pt-2">
+      <div className="">
+        <button
+          className=" border-1 p-1 rounded-md  border-r-2 bg-violet-400"
+          onClick={() => navigate("/LoginUser")}
+        >
+          Login
+        </button>
+      </div>
     </div>
   );
 }
 export default Header;
-
